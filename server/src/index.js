549240bdee3ba8708766056d401ac6165dd07d8f
Omkar Ghodekar <omkarghodekar140803@gmail.com>
2026-03-29 15:56:39 +0530

    fixes

--- server/src/index.js
+++ server/src/index.js
@@ -20,14 +20,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'http://localhost:5175',
-    'http://127.0.0.1:5173',
-    'http://127.0.0.1:5174',
-  ],
-  credentials: true,
+  origin: '*',
+  credentials: false,
 }));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
